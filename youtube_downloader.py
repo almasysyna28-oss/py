d455972601ff7a741c2721fcb9344f5e99d01ae4
almasysyna28-oss <almasysyna28@gmail.com>
2026-07-08 16:16:13 +0330

    Change message retrieval to sending a message

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -2,6 +2,10 @@ import requests
 
 TOKEN = "1624551307:sXvFHJ0-5wGM-VwHbGqW7DuLH0DUHNKZfP8"
 BASE_URL = f"https://tapi.bale.ai/bot{TOKEN}"
+CHAT_ID = 536174723
 
-resp = requests.get(f"{BASE_URL}/getUpdates")
+resp = requests.post(f"{BASE_URL}/sendMessage", json={
+    "chat_id": CHAT_ID,
+    "text": "سلام! این یه پیام تستیه 🎬"
+})
 print(resp.json())
